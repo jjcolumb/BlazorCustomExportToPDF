2cae6c7fe6e5e507790cb95aedb6f75e0f6a31a6
Jose Javier Columbie <josejaviercolumbie@yahoo.com>
2023-12-27 12:58:11 -0700

    Add project files.

---
+++ BlazorCustomExportToPDF.Blazor.Server/Controllers/CustomExportController.cs
@@ -0,0 +1,94 @@
+﻿using BlazorCustomExportToPDF.Module.BusinessObjects;
+using BlazorCustomExportToPDF.Module.Helpers;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Blazor.Editors;
+using DevExpress.ExpressApp.Blazor.Editors.Grid;
+using DevExpress.ExpressApp.Blazor.Editors.Models;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Persistent.Base;
+using DevExpress.Web.Data;
+using DevExpress.Web;
+using DevExpress.Xpo;
+using DevExpress.XtraPrinting;
+using DevExpress.XtraReports.UI;
+using System.Windows.Forms;
+using System.Diagnostics;
+using DevExpress.Utils.CommonDialogs.Internal;
+using DevExpress.ExpressApp.Blazor;
+using Microsoft.JSInterop;
+
+namespace BlazorCustomExportToPDF.Blazor.Server.Controllers
+{
+    public class CustomExportController : ViewController
+    {
+        SimpleAction customExport;
+        public CustomExportController()
+        {
+            customExport = new SimpleAction(this, "customExport", PredefinedCategory.Export);
+            customExport.Execute += customExport_Execute;
+            customExport.Caption = "Export PDF";
+        }
+        private void customExport_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            
+            var listEditor = ((ListView)View).Editor as DxGridListEditor;
+
+            if (listEditor != null)
+            {              
+                IDxGridAdapter adapter = listEditor.GetGridAdapter();
+                IList<string> propertyNames = new List<string>();
+
+                foreach (DxGridDataColumnModel columnModel in adapter.GridDataColumnModels)
+                {
+                    if (!String.IsNullOrWhiteSpace(columnModel.Caption))
+                    {
+                        if (columnModel.Visible)
+                            propertyNames.Add((columnModel).FieldName);                      
+                    }
+                }             
+
+                string fields = String.Empty;
+                foreach (var detail in propertyNames)
+                {
+                    fields += detail;
+                    if (detail != propertyNames.Last()) { fields += ";"; }
+                }
+
+                var dataView = ObjectSpace.CreateDataView(View.ObjectTypeInfo.Type, fields, null, null);
+
+                //Create Report
+                XtraReport report = new XtraReport();
+                report.DataSource = dataView;
+                ReportHelper.CreateReport(report, propertyNames.ToArray());
+                var reportName = View.ObjectTypeInfo.Name;
+                report.Name = reportName + " Report.pdf";
+                report.CreateDocument();
+
+                //Export PDF Options
+                PdfExportOptions pdfOptions = report.ExportOptions.Pdf;
+                pdfOptions.PdfACompatibility = PdfACompatibility.PdfA3b;
+                pdfOptions.DocumentOptions.Application = "Test Application";
+                pdfOptions.DocumentOptions.Author = "DX Documentation Team";
+                pdfOptions.DocumentOptions.Keywords = "Xari, Reporting, PDF";
+                pdfOptions.DocumentOptions.Producer = Environment.UserName.ToString();
+                pdfOptions.DocumentOptions.Subject = "Document Subject";
+                pdfOptions.DocumentOptions.Title = "Document Title";
+
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), report.Name);
+                report.ExportToPdf(path, pdfOptions);
+
+                
+
+                IJSRuntime jsRuntime = (Application as BlazorApplication).ServiceProvider.GetRequiredService<IJSRuntime>();
+
+                if (!String.IsNullOrEmpty(path))
+                {
+                     jsRuntime.InvokeVoidAsync("BlazorDownloadFile", report.Name, Convert.ToBase64String(File.ReadAllBytes(path)), ".pdf", "application/pdf");
+                }
+
+
+            }
+        }
+    }
+}
---
+++ BlazorCustomExportToPDF.Module/BusinessObjects/Customer.cs
@@ -0,0 +1,70 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace BlazorCustomExportToPDF.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    public class Customer : BaseObject
+    {
+        public Customer(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        string phone;
+        string email;
+        string country;
+        string lastName;
+        string firstName;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string FirstName
+        {
+            get => firstName;
+            set => SetPropertyValue(nameof(FirstName), ref firstName, value);
+        }
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string LastName
+        {
+            get => lastName;
+            set => SetPropertyValue(nameof(LastName), ref lastName, value);
+        }
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Country
+        {
+            get => country;
+            set => SetPropertyValue(nameof(Country), ref country, value);
+        }
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Email
+        {
+            get => email;
+            set => SetPropertyValue(nameof(Email), ref email, value);
+        }
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Phone
+        {
+            get => phone;
+            set => SetPropertyValue(nameof(Phone), ref phone, value);
+        }
+    }
+}
